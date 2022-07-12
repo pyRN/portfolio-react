f45611f29ad5f0bbea5fc348321b97f948b07a21
Jacob Mayeux <jacobmayeux@gmail.com>
2022-07-12 09:03:54 -0500

    Created Projects component, styled elements

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,5 +1,30 @@
-const Project = () => {
-  return <div>Project</div>;
+const Project = ({ oProject, aIcons }) => {
+  return (
+    <div
+      className="flex-column center project-card"
+      key={oProject.sProjectTitle}
+    >
+      <a
+        href={oProject.sUrl}
+        target="_blank"
+        rel="noreferrer"
+        className="project-url"
+      >
+        <h2 className="project-header">{oProject.sProjectTitle}</h2>
+      </a>
+      <p className="project-text">{oProject.sDescription}</p>
+      <a href={oProject.sUrl} target="_blank" rel="noreferrer">
+        <img
+          src={require(`./Images/${oProject.sProjectImg}`)}
+          alt="Food Template Site"
+          className="project-img"
+        />
+      </a>
+      <div className="flex-row center tech-stack">
+        {aIcons.filter((oIcon) => oProject.aIcons.includes(oIcon.props.name))}
+      </div>
+    </div>
+  );
 };
 
 export default Project;
--- src/Components/Projects/ProjectsContainer.jsx
+++ src/Components/Projects/ProjectsContainer.jsx
@@ -1,8 +1,69 @@
+import oInfo from "../info.json";
+
 //Components
 import Project from "./Project";
 
+//Icons
+import {
+  SiFirebase,
+  SiReact,
+  SiRedux,
+  SiJavascript,
+  SiHtml5,
+  SiCss3,
+} from "react-icons/si";
+
 const ProjectsContainer = () => {
-  return <div>ProjectsContainer</div>;
+  const { sTitle, aProjects } = oInfo.oProjects;
+  const aIcons = [
+    <SiReact
+      name="SiReact"
+      alt="SiReact"
+      className="tech-icon"
+      key="SiReact"
+    />,
+    <SiRedux
+      name="SiRedux"
+      alt="SiRedux"
+      className="tech-icon"
+      key="SiRedux"
+    />,
+    <SiJavascript
+      name="SiJavascript"
+      alt="SiJavascript"
+      className="tech-icon"
+      key="SiJavascript"
+    />,
+    <SiHtml5
+      name="SiHtml5"
+      alt="SiHtml5"
+      className="tech-icon"
+      key="SiHtml5"
+    />,
+    <SiCss3 name="SiCss3" alt="SiCss3" className="tech-icon" key="SiCss3" />,
+    <SiFirebase
+      name="SiFirebase"
+      alt="SiFirebase"
+      className="tech-icon"
+      key="SiFirebase"
+    />,
+  ];
+
+  return (
+    <section
+      id="projects-section"
+      className="flex-column section-card center projects-container"
+    >
+      <h2 className="section-heading">{sTitle}</h2>
+      {aProjects.map((oProject) => (
+        <Project
+          oProject={oProject}
+          aIcons={aIcons}
+          key={oProject.sProjectTitle}
+        />
+      ))}
+    </section>
+  );
 };
 
 export default ProjectsContainer;
