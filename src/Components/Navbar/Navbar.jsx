bd3b83413404a7a0c32440bf0889f64cb1ae29ce
Jacob Mayeux <jacobmayeux@gmail.com>
2022-07-12 09:01:34 -0500

    Created Navbar component, styled elements, added animation to links

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,40 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 import oInfo from "../info.json";
 import "./Styles/Navbar.css";
 
-//Multimedia
-import Close from "./Icons/close.svg";
+//Icons
+import { IoIosCloseCircle, IoIosMenu } from "react-icons/io";
 import FleurDeLis from "./Icons/fleurdelis.svg";
-import Hamburger from "./Icons/hamburger.svg";
-import Resume from "../../Multimedia/Documents/Jacob_Mayeux_Resume_6_2022.pdf";
+
+//Documents
+import Resume from "../Documents/Jacob_Mayeux_Resume_6_2022.pdf";
 
 const Navbar = () => {
   const { aNavbarLinks } = oInfo.oNavbar;
   const oHamburgerMenu = useRef(null);
+  const [bHideNavbar, fnSetHideNavbar] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", fnHandleScroll);
+    return () => window.removeEventListener("scroll", fnHandleScroll);
+  });
+
+  const fnHandleScroll = (event) => {
+    if (window !== undefined) {
+      let windowHeight = window.scrollY;
+      let windowWidth = window.innerWidth;
+      if (windowHeight > 150 && windowWidth > 750) {
+        if (bHideNavbar === false) fnSetHideNavbar(true);
+      } else if (windowHeight < 150 && windowWidth > 750) {
+        fnSetHideNavbar(false);
+      } else {
+        fnSetHideNavbar(false);
+      }
+    }
+  };
+
   return (
-    <nav className="flex-row navbar" id="navbar">
+    <nav className={`flex-row navbar ${bHideNavbar ? "hidden" : ""}`}>
       <img
         className="logo"
         src={FleurDeLis}
@@ -30,6 +52,7 @@ const Navbar = () => {
                 className="nav-link"
                 key={sNavLink}
                 target="_blank"
+                rel="noreferrer"
               >
                 {sNavLink}
               </a>
@@ -48,20 +71,18 @@ const Navbar = () => {
       </div>
 
       <div className="hamburger-menu close" ref={oHamburgerMenu}>
-        <img
+        <IoIosCloseCircle
           className="close-icon close"
-          src={Close}
-          alt="=Close SVG"
+          alt="=IoIosCloseCircle"
           width="50px"
           height="50px"
           onClick={() => {
             oHamburgerMenu.current.classList.toggle("open");
           }}
         />
-        <img
+        <IoIosMenu
           className="hamburger-icon close"
-          src={Hamburger}
-          alt="=Close SVG"
+          alt="IoIosMenu"
           width="50px"
           height="50px"
           onClick={() => {
